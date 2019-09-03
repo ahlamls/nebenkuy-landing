629b21c710377aeac169b931a3f3f2ea543937df
ahlamls <fulldoang@gmail.com>
2019-09-03 13:49:41 +0700

    modularisasi , pindah ke php , tambah item navbar

--- index.php
+++ index.php
@@ -1,37 +1,4 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<script src="https://use.fontawesome.com/releases/v5.9.0/js/all.js" data-auto-replace-svg="nest"></script>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
-    <link rel="stylesheet" href="style.css" >
-
-    <title>Nebenkuy</title>
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-<a class="navbar-brand mr-auto mr-lg-0 "  href="#"><img src="nebeng.png" class="navlogo">  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="https://nebenkuy.blogspot.com/">Blog</a>
-        </li>
-      </ul>
-
-  </div>
-</nav>
-<div class="container">
+<?php require_once "modular/header.php"?>
   <div class="headnyut ">
 <div class="row">
 
@@ -89,14 +56,4 @@
         <button class="btn btn-lg btn-success">Download Aplikasinya Sekarang!</button>
   </center>
     </div>
-
-    <hr>
-    Copyright 2019 - NEBENKUY
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" ></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" ></script>
-  </body>
-</html>
+    <?php require_once "modular/footer.php"?>
---
+++ modular/header.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<script src="https://use.fontawesome.com/releases/v5.9.0/js/all.js" data-auto-replace-svg="nest"></script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="style.css" >
+
+    <title>Nebenkuy</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand mr-auto mr-lg-0 "  href="#"><img src="nebeng.png" class="navlogo">  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item ">
+          <a class="nav-link" href="https://nebenkuy.blogspot.com/">Blog</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Tiket Transportasi</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="#">Ikut Tidur</a>
+        </li>
+
+
+        <li class="nav-item">
+          <a class="nav-link" href="../culayCMS">Titip</a>
+        </li>
+      </ul>
+
+  </div>
+</nav>
+<div class="container">
